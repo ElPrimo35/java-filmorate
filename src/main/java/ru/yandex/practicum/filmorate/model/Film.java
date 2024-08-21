3ec2a27ed4aa29742d1fb02ce11ed4b2d11f5848
El.primo <penny011.re@gmail.com>
2024-08-21 22:06:45 +0300

    10 sprint SecondVersion

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,9 +1,6 @@
 package ru.yandex.practicum.filmorate.model;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Past;
-import jakarta.validation.constraints.Positive;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 import lombok.Data;
 
 import java.time.LocalDate;
@@ -13,12 +10,12 @@ import java.time.LocalDate;
  */
 @Data
 public class Film {
-
     private int id;
     @NotBlank(message = "название фильма не должно быть пустым")
     private String name;
     @Size(max = 200, message = "Описание должно быть короче двухсот символов!")
     private String description;
+    @NotNull
     @Past
     private LocalDate releaseDate;
     @Positive(message = "Продолжительность фильма должна быть положительной")
